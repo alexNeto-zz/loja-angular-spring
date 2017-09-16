72974f69e92631ea19a15fc2cb3bc8929c6be8f9
alexNeto <alex.neto3@hotmail.com>
2017-09-16 01:04:34 -0300

    comit inicial

---
+++ angular-loja/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { AuthComponent } from './auth/auth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
